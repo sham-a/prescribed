0ca890956474d9a4cd02aa5d7462f23ccb760283
sham-a <ayahshamma123@gmail.com>
2021-04-18 18:04:41 -0400

    Added basic bottom navigation layout

---
+++ prescribed/app/src/main/java/com/example/prescribed/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.prescribed;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.example.prescribed.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
